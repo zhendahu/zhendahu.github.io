cb10006bd4da69bb5b404ebedfabed84cdbea3c4
Zhenda Hu <zhendahu@reffie.me>
2023-12-14 00:14:21 -0800

    Update hero page

--- zhenda-portfolio/src/app/components/HeroSection.jsx
+++ zhenda-portfolio/src/app/components/HeroSection.jsx
@@ -4,17 +4,22 @@ import Image from 'next/image'
 const HeroSection = () => {
   return (
     <section>
-        <div className="grid grid-cols-1 lg:grid-cols-12">
-            <div className="col-span-7 place-self-center">
-              <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
-                  Hello, I'm Zhenda. I'm a:
+        <div className="grid grid-cols-1 sm:grid-cols-12">
+            <div className="col-span-7 place-self-center text-center sm:text-left">
+              <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+                  <span className="text-[#64FFDA] text-base sm:text-1xl lg:text-2xl font-mono bg-clip-text">Hi, my name is </span> 
+                  Zhenda Hu. I'm a:
               </h1>
-              <p className="text-[#ADB7BE] text-lg mb-6 lg:text-xl">
+              <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
                   bakldsjfaklsdjfaklsdjf
               </p>
               <div>
-                <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black">Hire Me</button>
-                <button className='px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3'>Download Resume</button>
+                <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 to-green-500 hover:from-blue-500 hover:to-yellow-500 text-white">
+                  Hire Me
+                </button>
+                <button className='px-6 py-3 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 to-green-500 hover:from-blue-500 hover:to-yellow-500 text-white mt-3'>
+                  Download Resume
+                </button>
               </div>
             </div>
             <div className="col-span-5 place-self-center mt-4 lg:mt-0">
--- zhenda-portfolio/src/app/page.js
+++ zhenda-portfolio/src/app/page.js
@@ -3,8 +3,10 @@ import HeroSection from './components/HeroSection'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#152342] container mx-auto px-12 py-4">
-      <HeroSection />
+    <main className="flex min-h-screen flex-col bg-[#152342]">
+      <container class="container mx-auto px-12 py-4">
+        <HeroSection />
+      </container>
     </main>
   )
 }
