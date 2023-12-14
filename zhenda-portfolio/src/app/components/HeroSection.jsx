5dc5800e02c458de9ebc8ef6ef4719c33cc06e9b
Zhenda Hu <zhendahu@reffie.me>
2023-12-13 23:54:15 -0800

    Update HeroSection.jsx

--- zhenda-portfolio/src/app/components/HeroSection.jsx
+++ zhenda-portfolio/src/app/components/HeroSection.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 
 const HeroSection = () => {
   return (
@@ -8,12 +9,24 @@ const HeroSection = () => {
               <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
                   Hello, I'm Zhenda. I'm a:
               </h1>
-              <p className="text-[#ADB7BE] text-lg lg:text-xl">
+              <p className="text-[#ADB7BE] text-lg mb-6 lg:text-xl">
                   bakldsjfaklsdjfaklsdjf
               </p>
+              <div>
+                <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black">Hire Me</button>
+                <button className='px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3'>Download Resume</button>
+              </div>
             </div>
-            <div className="col-span-5">
-
+            <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+              <div className='rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative'>
+                <Image
+                  src="/images/zhenda_memoji.png"
+                  alt="hero image"
+                  className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
+                  width={300}
+                  height={300}
+                />
+              </div>
             </div>
         </div>
     </section>
