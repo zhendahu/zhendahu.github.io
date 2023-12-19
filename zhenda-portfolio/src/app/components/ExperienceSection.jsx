58fed1688711cbd23e1580cd7fb6432d8660e51d
Zhenda Hu <zhendahu@reffie.me>
2023-12-19 00:21:48 -0800

    start on experience section and tab

--- zhenda-portfolio/src/app/components/ExperienceSection.jsx
+++ zhenda-portfolio/src/app/components/ExperienceSection.jsx
@@ -1,10 +1,11 @@
 "use client"
 import React, { useTransition, useState } from 'react'
+import TabButton from './TabButton'
 
 const ExperienceSection = () => {
 
     const [tab, setTab] = useState("UCLA")
-    const [startTransition, isPending] = useTransition()
+    const [isPending, startTransition] = useTransition()
 
     const handleTabChange = (id) => {
         startTransition(() => {
@@ -13,23 +14,10 @@ const ExperienceSection = () => {
     }
 
   return (
-    <section className='text-white flex flex-col items-center justify-center'>
-        <h2 className='text-3xl font-mono text-[#64FFDA]'> <span className='text-white font-mono text-base'>02.</span> Experience</h2>
-        <hr class="h-px my-4 bg-gray-200 border-0 dark:bg-gray-700" />
-        <p className='text-[#ADB7BE] text-xs sm:text-sm mr-6 mb-4 lg:text-base'>
-                    Hey, thanks for stopping by! 
-                    <br></br>
-                    <br></br>
-                    My name is Zhenda Hu, currently a third year Computer Science student at UCLA. I've always viewed computing as the most powerful creative medium, in that your creations can cause the most impact with the least amount of resources. I started developing and creating applications first in the form of video games, as a bored high schooler during the pandemic, and have never looked back since. 
-                    <br></br>
-                    <br></br>
-                    Fast forward to today, my toolbox and maturity as a developer has expanded tenfold. I've had the opportunity to learn a wide variety of programming languages (from object oriented to functional, low level to high level), as well as the privilege to apply the knowledge in a variety of <span className='text-[#64FFDA]'>professional experiences</span> (from application engineering to full stack web development). 
-                    <br></br>
-                    <br></br>
-                    Here are a few technologies I've been working with recently:
-                    <br></br>
-                    <br></br>
-                </p>                                                                                                                                                                                                      
+    <section className=''>
+        <div className='md:grid md:grid-cols-2 gap-6 items-center py-8 px-2 xl:gap-6 sm:py-16 xl:px-16'>
+            <TabButton selectTab={() => handleTabChange("UCLA")} active={tab === "UCLA"}>UCLA</TabButton>
+        </div>                                                                                                                                                                                              
     </section>
   )
 }
--- zhenda-portfolio/src/app/page.js
+++ zhenda-portfolio/src/app/page.js
@@ -8,10 +8,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#152342]">
       <Navbar />
-      <container className="container mt-24 mx-auto px-12 py-20">
+      <container className="container mt-24 mx-auto px-12 "> {/* Maybe put py-20 on containers? Space them out a bit more?*/}
         <HeroSection />
       </container>
-      <container className="container mt-24 mx-auto px-12 py-20">
+      <container className="container mt-24 mx-auto px-12 ">
         <AboutSection />
       </container>
       <container>
