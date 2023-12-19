8b497a9acf9b68ab19eeff7f623c2d71940fe7c2
Zhenda Hu <zhendahu@reffie.me>
2023-12-19 00:25:09 -0800

    Update ExperienceSection.jsx

--- zhenda-portfolio/src/app/components/ExperienceSection.jsx
+++ zhenda-portfolio/src/app/components/ExperienceSection.jsx
@@ -15,6 +15,8 @@ const ExperienceSection = () => {
 
   return (
     <section className=''>
+            <h2 className='text-3xl font-mono text-[#64FFDA] flex justify-center'> <span className='text-white font-mono text-base flex justify-center'>02. </span> Experience</h2>
+        
         <div className='md:grid md:grid-cols-2 gap-6 items-center py-8 px-2 xl:gap-6 sm:py-16 xl:px-16'>
             <TabButton selectTab={() => handleTabChange("UCLA")} active={tab === "UCLA"}>UCLA</TabButton>
         </div>                                                                                                                                                                                              
