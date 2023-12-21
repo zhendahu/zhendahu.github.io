82fd0efcd21c110b5a77d172bf42593d59c030a0
Zhenda Hu <zhendahu@reffie.me>
2023-12-20 20:10:30 -0800

    tab buttons now work

--- zhenda-portfolio/src/app/components/ExperienceSection.jsx
+++ zhenda-portfolio/src/app/components/ExperienceSection.jsx
@@ -16,7 +16,7 @@ const ExperienceSection = () => {
   return (
     <div>
         <section className='mt-4 md:mt-0 text-left flex flex-col h-full px-2 items-center xl:px-16'>
-            <h2 className='text-3xl text-white font-bold'> <span className='font-mono text-[#64FFDA] text-base'>02.</span> Experience</h2>
+            <h2 className='text-3xl text-white font-bold'> <span className='font-mono text-[#64FFDA] text-base'>02.</span> Work Experience</h2>
         </section>
         
         <div className='items-center flex flex-col'>
@@ -24,10 +24,9 @@ const ExperienceSection = () => {
         </div>
         <section className='md:grid md:grid-cols-2 gap-6 items-center py-4 px-2 xl:gap-6 sm:py-8 xl:px-16'>
             <div className='flex flex-col gap-6'>
-                <div class="inline-block h-[250px] min-h-[1em] w-0.5 self-stretch bg-neutral-100 opacity-100 dark:opacity-50"></div>
-                <TabButton selectTab={() => handleTabChange("UCLA")} active={tab === "UCLA"}>UCLA</TabButton>
-                <TabButton selectTab={() => handleTabChange("Reffie")} active={tab === "Reffie"}>Reffie</TabButton>
-                <TabButton selectTab={() => handleTabChange("MKSemi")} active={tab === "MKSemi"}>MKSemi</TabButton>
+                <TabButton selectTab={() => handleTabChange("ucla")} active={tab === "ucla"}>UCLA</TabButton>
+                <TabButton selectTab={() => handleTabChange("reffie")} active={tab === "reffie"}>Reffie</TabButton>
+                <TabButton selectTab={() => handleTabChange("mksemi")} active={tab === "mksemi"}>MKSemi</TabButton>
             </div>
         </section>
     </div>
--- zhenda-portfolio/src/app/components/TabButton.jsx
+++ zhenda-portfolio/src/app/components/TabButton.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const TabButton = ({ active, selectTab, children }) => {
-const buttonClasses = active ? 'text-white border-b border-purple-500' : 'text-[#ADB7BE]'
+    const buttonClasses = active ? 'text-[#64FFDA] border-b border-white ' : 'text-[#ADB7BE]'
 
   return (
     <button onClick={selectTab}>
-        <p className={'mr-3 font-mono text-base hover:text-[#64FFDA] text-[#ADB7BE] ${buttonClasses}'}>
+        <p className={`mr-3 font-mono text-base hover:text-[#64FFDA] ${buttonClasses}`}>
         {children}
         </p>
         
