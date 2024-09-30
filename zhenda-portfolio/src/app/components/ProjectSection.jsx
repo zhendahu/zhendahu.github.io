16d2cdd59b66b957ee5513f80b4919840fb58f12
Zhenda Hu <zhendahu@reffie.me>
2024-09-29 17:07:57 -0700

    continue with project section, finish mobile view

--- zhenda-portfolio/src/app/components/ProjectSection.jsx
+++ zhenda-portfolio/src/app/components/ProjectSection.jsx
@@ -5,6 +5,7 @@ import Image from 'next/image'
 import convo from '../../../public/images/convo.png'
 import ecoscan from '../../../public/images/ecoscan.png'
 import flappy_bird from '../../../public/images/flappy_bird_ai.png'
+import arrowupright from '../../../public/images/arrowupright.svg'
 
 const TAB_DATA = [
     {
@@ -49,39 +50,49 @@ const TAB_DATA = [
 
 const ProjectSection = () => {
    return(
-    <div>
-        <div className="container">
-            <div className='flex justify-center'>
-                <p className='uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-center bg-clip-text text-transparent'>Real-world Results</p>
-            </div>
-            <div className='flex justify-center'>
-                <h2 className='text-3xl text-white font-bold mt-6'> <span className='font-mono text-[#64FFDA] text-base'>03.</span> Featured Projects</h2>
-            </div>
-            
-            <p>See how I transformed concepts into engaging digital experiences.</p>
-            <div>
-                {TAB_DATA.map(project => (
-                    <div key={project.title}>
-                        <div>
-                            <span>{project.company}</span>
-                            <span>{project.year}</span>
+    <section className='pb-16'>
+        <div>
+            <div className="container">
+                <div className='flex justify-center'>
+                    <p className='uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-center bg-clip-text text-transparent'>Real-world Results</p>
+                </div>
+                <div className='flex justify-center'>
+                    <h2 className='text-3xl text-white font-bold mt-6'> <span className='font-mono text-[#64FFDA] text-base'>03.</span> Featured Projects</h2>
+                </div>
+
+                <div className='flex flex-col mt-10 gap-20 justify-center'>
+                    {TAB_DATA.map(project => (
+                        <div key={project.title} className="bg-[#202b43] rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 pl-8 pr-8 pt-8 text-white">
+                            <div className='flex'>
+                                <div className='bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text'>
+                                    <span>{project.company}</span>
+                                    <span>&bull;</span>
+                                    <span>{project.year}</span>
+                                </div>
+                            </div>
+                            <h3 className='font-mono text-2xl mt-2'>{project.title}</h3>
+                            <hr className='border-t-2 border-white/5 mt-4'></hr>
+                            <hr />
+                            <ul className='flex flex-col gap-4 mt-4'>
+                                {project.results.map(result => (
+                                    <li className='flex gap-2 text-sm'>
+                                        <span className='text-[#64FFDA]'>▹</span> <span>{result.title}</span>
+                                    </li>
+                                ))}
+                            </ul>
+                            <a href={project.link}>
+                                <button className='bg-white text-gray-950 h-12 w-full rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8'> 
+                                    <span>View Project</span>
+                                    <arrowupright></arrowupright>
+                                </button>
+                            </a>
+                            <Image src={project.image} alt={project.title} className='mt-8 -mb-4'/>
                         </div>
-                        <h3>{project.title}</h3>
-                        <hr />
-                        <ul>
-                            {project.results.map(result => (
-                                <li>{result.title}</li>
-                            ))}
-                        </ul>
-                        <a href={project.link}>
-                            <button> View Project</button>
-                        </a>
-                        <Image src={project.image} alt={project.title}/>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
         </div>
-    </div>
+    </section>
    );
 }
 
